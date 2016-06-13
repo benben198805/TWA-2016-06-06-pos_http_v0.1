b5d4ca188a533e6ee241360927c87e363f571404
benben198805 <benben198805@126.com>
2016-06-13 13:23:47 +0800

    beautify ui

--- js/show_good_list/view/render_subTotal.js
+++ js/show_good_list/view/render_subTotal.js
@@ -1,14 +1,14 @@
 var renderSubTotal = {
     loadTemplate: function () {
-        var subTotalTitle = _.template("<div/><p><没钱赚商店>购物清单</p><p><%- time%></p></div>");
+        var subTotalTitle = _.template("<div class='panel panel-primary'><div class='panel-heading'><p><没钱赚商店>购物清单 <%- time%></p></div>");
 
-        var subTotalList = _.template("<div><ul class='list-group'><% _.forEach(billItems,function(item){%><li class='list-group-item'><%- item.name%>:<%- item.count%> 单价：<%- item.price.toFixed(2)%>per<%- item.unit%>  小计：<%- item.subtotal.toFixed(2)%></li><%})%></ul></div>");
+        var subTotalList = _.template("<div class='panel-body'><ul class='list-group'><% _.forEach(billItems,function(item){%><li class='list-group-item'><%- item.name%>:<%- item.count%> 单价：<%- item.price.toFixed(2)%>per<%- item.unit%>  小计：<%- item.subtotal.toFixed(2)%></li><%})%></ul>");
 
         var subTotalPomotionTitle = _.template("<div><p>挥泪赠送商品：</p></div>");
 
-        var subTotalPromotionList = _.template("<div><ul class='list-group'><% _.forEach(freeItems,function(item){%><li class='list-group-item'>名称：<%- element.name%>，数量：<%- element.freeCount%><%- element.unit%></li><%})%></ul></div>");
+        var subTotalPromotionList = _.template("<ul class='list-group'><% _.forEach(freeItems,function(element){%><li class='list-group-item'>名称：<%- element.name%>，数量：<%- element.freeCount%><%- element.unit%></li><%})%></ul>");
 
-        var subTotalSum = _.template("<div><p>总计：<%- total.toFixed(2)%></p><p>节省：<%- freeTotal.toFixed(2)%></p></div>");
+        var subTotalSum = _.template("</div><div class='panel-footer'><p>总计：<%- total.toFixed(2)%></p><p>节省：<%- freeTotal.toFixed(2)%></p></div></div>");
 
         return {
             subTotalTitle: subTotalTitle,
@@ -18,7 +18,7 @@ var renderSubTotal = {
             subTotalSum: subTotalSum
         }
     },
-    renderTemplate: function (receipt,targetDom) {
+    renderTemplate: function (receipt, targetDom) {
         var template = this.loadTemplate();
         var html = "";
 
